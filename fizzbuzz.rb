0a59177bf46a233c31838214bb2fc4b6ee99bae3
Will Andre <wandre94@gmail.com>
2020-11-25 01:58:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "fizzbuzz"
+    "FizzBuzz"
   elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
