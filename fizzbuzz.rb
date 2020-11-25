5c424c3a4a3901d6f14ffe394520b39fc2b0b025
Will Andre <wandre94@gmail.com>
2020-11-25 01:05:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0
+    put "Fizz"
+  elsif int % 5 == 0
+    put "Buzz"
+  else
+    put "nil"
+        
+end
